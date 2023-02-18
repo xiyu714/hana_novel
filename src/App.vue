c1864ef211b865431a63e4d48327269411b631f5
pomeloten <pomelo_cloud@outlook.com>
2023-02-18 21:33:26 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,90 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="body">
+    <header>
+      <HomeView/>
+      <RouterView />
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+header{
+  background-color: blue;
+  width: auto;
+  height: 71px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+/*header {*/
+/*  line-height: 1.5;*/
+/*  max-height: 100vh;*/
+/*}*/
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+/*.logo {*/
+/*  display: block;*/
+/*  margin: 0 auto 2rem;*/
+/*}*/
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+/*nav {*/
+/*  width: 100%;*/
+/*  font-size: 12px;*/
+/*  text-align: center;*/
+/*  margin-top: 2rem;*/
+/*}*/
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+/*nav a.router-link-exact-active {*/
+/*  color: var(--color-text);*/
+/*}*/
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+/*nav a.router-link-exact-active:hover {*/
+/*  background-color: transparent;*/
+/*}*/
 
-nav a:first-of-type {
-  border: 0;
-}
+/*nav a {*/
+/*  display: inline-block;*/
+/*  padding: 0 1rem;*/
+/*  border-left: 1px solid var(--color-border);*/
+/*}*/
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+/*nav a:first-of-type {*/
+/*  border: 0;*/
+/*}*/
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+/*@media (min-width: 1024px) {*/
+/*  header {*/
+/*    display: flex;*/
+/*    place-items: center;*/
+/*    padding-right: calc(var(--section-gap) / 2);*/
+/*  }*/
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/*  .logo {*/
+/*    margin: 0 2rem 0 0;*/
+/*  }*/
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+/*  header .wrapper {*/
+/*    display: flex;*/
+/*    place-items: flex-start;*/
+/*    flex-wrap: wrap;*/
+/*  }*/
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+/*  nav {*/
+/*    text-align: left;*/
+/*    margin-left: -1rem;*/
+/*    font-size: 1rem;*/
+
+/*    padding: 1rem 0;*/
+/*    margin-top: 1rem;*/
+/*  }*/
+/*}*/
 </style>
