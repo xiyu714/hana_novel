20d71c5e15a4b57880207385ea9bbcc4b719be9b
xiyu <daixiyu714@outlook.com>
2023-02-19 10:52:30 +0800

    创建一个很简单的中间件

--- back_end/main.mjs
+++ back_end/main.mjs
@@ -1,9 +1,21 @@
 import Koa from "koa"
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
 // response
 app.use(ctx => {
     ctx.body = 'Hello Koa';
 });
 
-app.listen(30000);
+
+let port = 30000;
+
+app.listen(port);
+
+console.log(`应用启动成功: http://localhost:${port}`)
