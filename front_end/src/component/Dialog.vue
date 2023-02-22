5d88b846af5fed08d07fab46d02cf210f651efff
xiyu <daixiyu714@outlook.com>
2023-02-22 22:41:33 +0800

    添加对话框组件

---
+++ front_end/src/component/Dialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="{dialog_visible: dialog_visible}" class="dialog">
+    <div class="dialog__body">
+      <div>
+        <slot></slot>
+      </div>
+      <div @click="dialog_visible = false" style="position: absolute; right: 10px; top: 10px;">
+        <icon-close theme="outline" size="24" fill="#333"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let dialog_visible = ref(true);
+
+defineExpose({
+  dialog_visible
+})
+</script>
+
+<style>
+.dialog_visible {
+  display: block !important;
+}
+
+.dialog {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #333333ee;
+  display: none;
+}
+
+.dialog__body {
+  background: white;
+  width: 80%;
+  margin-left: 10%;
+  height: 500px;
+  margin-top: 100px;
+  position: relative;
+}
+
+</style>
