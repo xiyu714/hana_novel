0bdcd2a99e058be266f6037c752aa5a1de152e8f
xiyu <daixiyu714@outlook.com>
2023-02-22 23:20:29 +0800

    修改对话框默认大小 hover时改变颜色（未完成）

--- front_end/src/component/Dialog.vue
+++ front_end/src/component/Dialog.vue
@@ -1,10 +1,10 @@
 <template>
   <div :class="{dialog_visible: dialog_visible}" class="dialog">
-    <div class="dialog__body">
+    <div class="dialog__body" style="width: 500px;height: 450px; margin-left: 30%;">
       <div>
         <slot></slot>
       </div>
-      <div @click="dialog_visible = false" style="position: absolute; right: 10px; top: 10px;">
+      <div class="close_button" @click="dialog_visible = false" style="position: absolute; right: 10px; top: 10px; cursor: pointer;">
         <icon-close theme="outline" size="24" fill="#333"/>
       </div>
     </div>
@@ -22,6 +22,11 @@ defineExpose({
 </script>
 
 <style>
+
+.close_button:hover {
+  color: red;
+}
+
 .dialog_visible {
   display: block !important;
 }
@@ -34,6 +39,7 @@ defineExpose({
   height: 100vh;
   background-color: #333333ee;
   display: none;
+  z-index: 1000;
 }
 
 .dialog__body {
