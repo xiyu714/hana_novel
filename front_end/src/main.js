3a69becf952665be96a2976f93438436b63d8e6e
xiyu <daixiyu714@outlook.com>
2023-02-28 21:50:50 +0800

    添加element UI

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 import {install} from '@icon-park/vue-next/es/all';
@@ -7,6 +11,8 @@ import { api } from "./api"
 import './assets/main.css'
 
 const app = createApp(App)
+// 使用element ui
+app.use(ElementPlus)
 install(app);
 
 
