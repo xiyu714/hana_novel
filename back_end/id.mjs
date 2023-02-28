d28e0a2c04ed44730dcdfde87d49a06a5112ed73
xiyu <daixiyu714@outlook.com>
2023-02-28 21:55:04 +0800

    修改chapterid长度

--- back_end/id.mjs
+++ back_end/id.mjs
@@ -1,12 +1,13 @@
 import {customAlphabet, nanoid} from 'nanoid'
 
 const nanoId = customAlphabet('0123456789', 8);
+const chapterId = customAlphabet('0123456789', 10);
 
 export let get_book_id = () => {
     return Number(nanoId())
 }
 
 export let get_chapter_id = () => {
-    return Number(nanoId())
+    return Number(chapterId())
 }
 
