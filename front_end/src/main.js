b4ae7a4ec5b7902a2d3ddb561bff878875e4712a
pomeloten <pomelo_cloud@outlook.com>
2023-03-06 22:46:11 +0800

    安装pinia

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -7,12 +7,16 @@ import App from './App.vue'
 import router from './router'
 import { api } from "./api"
 
+import { createPinia } from 'pinia'
+
 import './assets/main.css'
+
+const pinia = createPinia()
 
 const app = createApp(App)
 // 使用element ui
 app.use(ElementPlus)
-
+app.use(pinia)
 
 app.use(router)
 
