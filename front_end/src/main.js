c18de19882c8b734cd6aca574722573cb5b3fc58
pomeloten <pomelo_cloud@outlook.com>
2023-03-08 23:52:14 +0800

    添加章节样式+格式化日期

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import "./extend"
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
--- front_end/src/views/Chapter.vue
+++ front_end/src/views/Chapter.vue
@@ -1,8 +1,16 @@
 <template>
-  <div v-if="!isLoading">
-    <h1>{{chapter.title}}</h1>
-    <div v-html="chapter.content"></div>
+  <div class="flex_center" style="background-color: #eae4d7; ">
+    <div v-if="!isLoading" style="width: 55%;background-color: #f6f1e7;padding: 80px 60px 30px 60px">
+      <div style="font-size: 24px;font-weight: bold;margin-bottom: 30px">{{book.chapter.title}}</div>
+      <div class="flex" style="margin-bottom: 40px;font-size: 12px">
+        <div >{{book.title}}</div>
+        <div style="margin: 0 10px">{{book.author}}</div>
+        <div>{{(new Date(book.chapter.updated_time)).pattern("yyyy-MM-dd hh:mm:ss")}}</div>
+      </div>
+      <div v-html="book.content" style="font-size: 16px"></div>
+    </div>
   </div>
+
 </template>
 
 <script setup>
@@ -13,15 +21,17 @@ import {ref} from "vue";
 
 const route = useRoute()
 let isLoading = ref(true);
-let chapter = ref(null);
+let book = ref(null);
 
 axios.post("book/content", {
   id: route.params.chapter_id,
   book_id: route.params.id
 }).then(({data}) => {
-  let chapter_data = data.data;
-  chapter_data.content = chapter_data.content.replaceAll("\n", "</br></br>");
-  chapter.value = chapter_data;
+  let book_data = data.data;
+  let ident = "&nbsp;&nbsp;&nbsp;&nbsp;";
+  book_data.content = ident + book_data.chapter.content.replaceAll("\n", "</br></br>" + ident);
+
+  book.value = book_data;
 }).finally(() => {
   isLoading.value = false
 })
