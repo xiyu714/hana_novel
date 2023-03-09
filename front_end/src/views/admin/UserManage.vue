97434cc08e62b3aec81c4a399af30baedc6832ca
pomeloten <pomelo_cloud@outlook.com>
2023-03-09 23:12:38 +0800

    增加添加用户功能

--- front_end/src/views/admin/UserManage.vue
+++ front_end/src/views/admin/UserManage.vue
@@ -4,13 +4,106 @@
       <el-input
           style="width: 300px;margin-right: 12px"
       ></el-input>
-      <el-button type="primary">搜索</el-button>
+      <el-button type="primary">
+        搜索
+
+      </el-button>
     </div>
     <div style="float: right">
       <el-button type="success">
         导出数据
       </el-button>
-      <el-button type="primary">添加用户</el-button>
+      <el-button type="primary" @click="dialogAddUser = true">添加用户</el-button>
+<!--      添加用户弹窗-->
+      <el-dialog v-model="dialogAddUser" title="添加用户">
+        <el-form
+            ref="AddForm"
+            label-width="90px"
+            :model="AddUser"
+            class="demo-dynamic"
+        >
+          <el-form-item
+              prop="name"
+              label="用户名:"
+              :rules="[
+                      {
+                        required: true,
+                        message: '请输入用户名',
+                        trigger: 'blur',
+                      }
+                    ]"
+          >
+            <el-input style="width: 220px;" v-model="AddUser.name" placeholder="请输入用户名"/>
+          </el-form-item>
+          <el-form-item
+              prop="email"
+              label="邮箱:"
+              :rules="[
+                      {
+                        required: true,
+                        message: '请输入邮箱',
+                        trigger: 'blur',
+                      },
+                      {
+                        type: 'email',
+                        message: '请输入正确的邮箱格式',
+                        trigger: ['blur', 'change'],
+                      },
+                    ]"
+          >
+            <el-input style="width: 220px;" v-model="AddUser.email" placeholder="请输入邮箱"/>
+          </el-form-item>
+
+<!--          <el-form-item-->
+<!--              prop="permission"-->
+<!--              label="用户权限:"-->
+<!--              :rules="[-->
+<!--                      {-->
+<!--                        required: true,-->
+<!--                        message: '请选择用户权限',-->
+<!--                        trigger: 'blur'-->
+<!--                      }-->
+<!--                    ]"-->
+<!--          >-->
+<!--            <el-select v-model="AddUser.permission" placeholder="请选择用户权限"  style="width: 220px;">-->
+<!--              <el-option label="普通用户" value="user"></el-option>-->
+<!--              <el-option label="管理员" value="admin"></el-option>-->
+<!--            </el-select>-->
+<!--          </el-form-item>-->
+
+
+          <el-form-item
+              prop="password"
+              label="密码:"
+              :rules="[
+                      {
+                        required: true,
+                        message: '请输入6-8位密码',
+                        trigger: 'blur',
+                      },{
+                          min:6,
+                          max:10,
+                          trigger: 'blur',
+                          message: '请输入6-8位密码',
+                      }
+                    ]"
+          >
+            <el-input type="password" style="width: 220px;" v-model="AddUser.password" placeholder="请输入6-8位密码"/>
+          </el-form-item>
+<!--          <el-button style="width: 220px;margin:8px 0 10px 90px;" @click="Add_User(AddForm)">注册</el-button>-->
+        </el-form>
+
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button @click="dialogAddUser = false">取消</el-button>
+            <el-button type="primary" @click="dialogAddUser = false;Add_User(AddForm)" >
+              添加用户
+            </el-button>
+          </span>
+        </template>
+
+      </el-dialog>
+
     </div>
   </div>
 <!--  用户信息表格-->
@@ -26,11 +119,12 @@
         style="width: 100%;"
     >
       <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column prop="id" label="用户ID" width="120" />
+      <el-table-column prop="name" label="用户名" width="120" />
+      <el-table-column prop="email" label="邮箱" width="120"/>
+<!--      <el-table-column prop="permission" label="用户权限" width="180" />-->
+      <el-table-column prop="avatar" label="头像url"  />
 
-      <el-table-column prop="name" label="用户名" width="180" />
-      <el-table-column prop="email" label="邮箱" width="220"/>
-      <el-table-column prop="avatar" label="头像url" width="220" />
-      <el-table-column prop="permission" label="用户权限" width="180" />
       <el-table-column prop="permission" label="用户状态" width="180" >
 
       </el-table-column>
@@ -39,57 +133,74 @@
       <el-table-column fixed="right" width="200" label="操作">
         <template v-slot="scope">
           <el-button style="padding: 3px;width: 70px;margin-right: 10px" v-model="scope.row.Id" type="primary">编辑</el-button>
-          <el-button style="padding: 3px;width: 70px" type="danger">删除</el-button>
+          <el-button style="padding: 3px;width: 70px" type="danger" @click="Delete_User">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
+<!--    编辑弹窗-->
+
   </div>
-
-
-<!--  <div style="display: inline-block;width: 100%">-->
-<!--    <div v-for="item in users"-->
-<!--      style="width: 270px;background-color: #ffffff;display: inline-block;-->
-<!--      margin: 30px 20px 0 10px;padding: 10px;box-shadow: 0 0 8px #E1DFDF">-->
-
-<!--      <div style="height: 25px;border-bottom: 1px solid #999;margin-bottom: 10px">-->
-<!--        <div style="float: left">{{item.name}}</div>-->
-<!--        <div style="float: right;cursor: pointer">-->
-<!--          <Delete style="width: 1em; height: 1em; margin-right: 8px;color: maroon" />-->
-<!--        </div>-->
-<!--      </div>-->
-
-<!--      <div style="font-size: 14px;">-->
-<!--        <div style="margin-left: 100px;margin-bottom: 10px">-->
-<!--          <img :src="item.avatar" style="width: 70px;height: 70px">-->
-<!--        </div>-->
-<!--        <div>用户名：{{item.name}}</div>-->
-<!--        <div style="margin-top: 2px">邮箱：{{item.email}}</div>-->
-<!--        <div style="margin-top: 2px">创建时间：{{item.created_time}}</div>-->
-<!--        <div style="margin-top: 2px">权限：{{item.permission}}</div>-->
-<!--        <div>-->
-<!--          用户状态：-->
-<!--          <el-switch-->
-<!--              v-model="value"-->
-<!--              class="mb-2"-->
-<!--              style="&#45;&#45;el-switch-on-color: #13ce66; &#45;&#45;el-switch-off-color: #ff4949"-->
-<!--              active-text="启用"-->
-<!--              inactive-text="禁用"-->
-<!--          />-->
-<!--        </div>-->
-<!--        <div>备注：</div>-->
-<!--      </div>-->
-
-
-
-<!--    </div>-->
-<!--  </div>-->
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import {reactive, ref,getCurrentInstance} from "vue";
 import {useRoute} from "vue-router";
 import {axios} from "../../api";
 import {Delete} from '@element-plus/icons-vue'
+
+//删除用户
+
+
+
+
+
+
+//注册弹窗
+const dialogAddUser = ref(false)
+const { proxy } = getCurrentInstance();
+const AddForm = ref(null);
+const AddUser = reactive({
+  email:'',
+  name:'',
+  password:''
+})
+
+const Add_User = (AddForm) =>{
+  AddForm.validate((valid) => {
+    if(valid){
+      axios.post("/user/register",AddUser)
+          .then(res => {
+            //注册成功
+            console.log(res)
+            if(res.data.status_code === 200){
+              AddForm.resetFields()
+              proxy.$message({
+                message:'账号注册成功，请登录！',
+                type:"success"
+              });
+            }else {
+              proxy.$message.error({
+                message:res.data.message
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err)
+            proxy.$message.error("注册失败！")
+          });
+    }else {
+      proxy.$message({
+        type:"error",
+        message:"表单填写错误"
+      });
+      return false
+    }
+  })
+}
+
+
+
+
 
 const value = ref(true)
 const usersData =ref([])
