0d7a0082a92bfeb0eea343ac6e0a70359848f231
pomeloten <pomelo_cloud@outlook.com>
2023-04-04 23:25:21 +0800

    我的书架大致样子(未完成)

--- front_end/src/views/BookShelf.vue
+++ front_end/src/views/BookShelf.vue
@@ -1,11 +1,63 @@
 <template>
-这是我的书架
+  <div style="background-color: #f7f6f2;height: 100%;width: 100%">
+  <div style="margin: 0 13% 0 13%;padding-top: 40px;background-color: #c45656">
+<!--    导航-->
+    <div style="float:left;width: 200px;">
+      <ul style="padding: 0;margin: 0;list-style-type: none">
+        <li style="padding: 10px 0 10px 30px;margin: 0;background-color: #529b2e" >
+            <span style="background-color: #c0c4cc">
+              <router-link to="/bookshelf">书架详情</router-link>
+            </span>
+        </li>
+        <li style="padding: 10px 0 10px 30px;margin-top: 10px;background-color: #529b2e" >
+            <span style="background-color: #c0c4cc">
+              <router-link to="/bookshelf/historybook">最近阅读</router-link>
+            </span>
+        </li>
+
+      </ul>
+    </div>
+    <div style="margin-left: 250px;">
+      <div v-if="$route.path === '/bookshelf'">
+<!--        搜索框-->
+        <div style="display: inline">
+          <el-input style="width: 300px" placeholder="请输入书本名或作者名搜索"></el-input>
+          <el-button type="primary">搜索</el-button>
+
+<!--          编辑-->
+
+
+        </div>
+<!--    书架 书本详情-->
+        <div style="display: inline-block;background-color: #529b2e;margin-top: 20px">
+
+          <div style="display: inline-block;cursor: pointer;margin:0 30px 20px 0;text-align: center" v-for="item in books"
+               @click="$router.push({ path: `/book/${item.id}` }) ">
+
+              <img style="width: 140px;height: 180px;border-radius: 3px" :src="item.cover_url">
+              <div>{{item.title}}</div>
+              <div>1章/章</div>
+
+          </div>
+
+
+
+        </div>
+      </div>
+      <router-view></router-view>
+    </div>
+  </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "BookShelf"
-}
+<script setup>
+import {ref} from "vue";
+import {axios} from "../api";
+
+const books = ref([])
+axios.post("book/list").then(({data}) => {
+  books.value = data.data;
+})
 </script>
 
 <style scoped>
