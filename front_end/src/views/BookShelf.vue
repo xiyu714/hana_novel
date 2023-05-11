e2a209ab52dc9bab0408e40517fb70ac90043ec1
pomeloten <pomelo_cloud@outlook.com>
2023-05-11 09:30:05 +0800

    在进入我的书架前进行登录验证

--- front_end/src/views/BookShelf.vue
+++ front_end/src/views/BookShelf.vue
@@ -1,16 +1,17 @@
 <template>
-  <div style="background-color: #f7f6f2;height: 100%;width: 100%">
-  <div style="margin: 0 13% 0 13%;padding-top: 40px;background-color: #c45656">
+  <div v-if="globalStore.user === undefined" style="height: 800px"></div>
+  <div v-else style="background-color: #f7f6f2;height: 100%;width: 100%">
+  <div style="margin: 0 13% 0 13%;padding-top: 40px;height: 800px">
 <!--    导航-->
     <div style="float:left;width: 200px;">
       <ul style="padding: 0;margin: 0;list-style-type: none">
-        <li style="padding: 10px 0 10px 30px;margin: 0;background-color: #529b2e" >
-            <span style="background-color: #c0c4cc">
+        <li style="padding: 10px 0 10px 30px;margin: 0;" >
+            <span >
               <router-link to="/bookshelf">书架详情</router-link>
             </span>
         </li>
-        <li style="padding: 10px 0 10px 30px;margin-top: 10px;background-color: #529b2e" >
-            <span style="background-color: #c0c4cc">
+        <li style="padding: 10px 0 10px 30px;margin-top: 10px;" >
+            <span>
               <router-link to="/bookshelf/historybook">最近阅读</router-link>
             </span>
         </li>
@@ -29,7 +30,7 @@
 
         </div>
 <!--    书架 书本详情-->
-        <div style="display: inline-block;background-color: #529b2e;margin-top: 20px">
+        <div style="display: inline-block;margin-top: 20px">
 
           <div style="display: inline-block;cursor: pointer;margin:0 30px 20px 0;text-align: center" v-for="item in books"
                @click="$router.push({ path: `/book/${item.id}` }) ">
@@ -48,16 +49,34 @@
     </div>
   </div>
   </div>
+
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {getCurrentInstance, ref} from "vue";
 import {axios} from "../api";
+import {useGlobalStore} from "../store";
+
+
+const { proxy } = getCurrentInstance();
+const globalStore = useGlobalStore();
+//判断用户有没有登录
+if(globalStore.user === undefined){
+  proxy.$message({
+    message:'请先登录',
+    type:"error"
+  });
+}
+
 
 const books = ref([])
+
 axios.post("book/list").then(({data}) => {
   books.value = data.data;
 })
+
+
+
 </script>
 
 <style scoped>
